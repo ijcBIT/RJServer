c9eed2479271792a2028b01b9b8a37a0e3ef1882
emiliolario <zelk19@gmail.com>
2024-07-03 10:47:52 +0200

    Update start_rstudio_server.sh image name

--- start_rstudio_server.sh
+++ start_rstudio_server.sh
@@ -2,7 +2,7 @@
 
 export SLUMR_SERVER_NAME="RStudio Server"
 export SLUMR_SERVER_SCRIPT="./bin/rstudio_server.slm"
-export SLUMR_SERVER_CONTAINER_NAME="rstudio_latest.sif"
+export SLUMR_SERVER_CONTAINER_NAME="rstudio_20240701.sif"
 export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/rstudio:20240701"
 export SLUMR_SERVER_LOG_FOLDER="rstudio_server"
 
