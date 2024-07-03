91add24fcc5cecdb510449686ab836cfbddb2532
Emilio <elario@carrerasresear.org>
2024-07-03 13:17:32 +0200

    Change rstudio server image to tidiverse

--- start_rstudio_server.sh
+++ start_rstudio_server.sh
@@ -2,8 +2,8 @@
 
 export SLUMR_SERVER_NAME="RStudio Server"
 export SLUMR_SERVER_SCRIPT="./bin/rstudio_server.slm"
-export SLUMR_SERVER_CONTAINER_NAME="rstudio_20240701.sif"
-export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/rstudio:20240701"
+export SLUMR_SERVER_CONTAINER_NAME="tidyverse_4.4.1.sif"
+export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/tidiverse:4.4.1"
 export SLUMR_SERVER_LOG_FOLDER="rstudio_server"
 
 
