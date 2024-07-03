312c7cf83f071c57f1f5b31577128d6690cea09e
Emilio <elario@carrerasresear.org>
2024-07-03 13:23:31 +0200

    fix typo in tidyverse

--- start_rstudio_server.sh
+++ start_rstudio_server.sh
@@ -3,7 +3,7 @@
 export SLUMR_SERVER_NAME="RStudio Server"
 export SLUMR_SERVER_SCRIPT="./bin/rstudio_server.slm"
 export SLUMR_SERVER_CONTAINER_NAME="tidyverse_4.4.1.sif"
-export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/tidiverse:4.4.1"
+export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/tidyverse:4.4.1"
 export SLUMR_SERVER_LOG_FOLDER="rstudio_server"
 
 
