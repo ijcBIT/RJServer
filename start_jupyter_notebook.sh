ae05bcb2a173d8cbcb279bcc168b448e13d87284
emiliolario <zelk19@gmail.com>
2024-07-03 10:48:26 +0200

    Update start_jupyter_notebook.sh image name

--- start_jupyter_notebook.sh
+++ start_jupyter_notebook.sh
@@ -2,7 +2,7 @@
 
 export SLUMR_SERVER_NAME="Jupyter Notebook"
 export SLUMR_SERVER_SCRIPT="./bin/jupyter_notebook.slm"
-export SLUMR_SERVER_CONTAINER_NAME="datascience-notebook_2024-06-24.sif"
+export SLUMR_SERVER_CONTAINER_NAME="jupyter_20240701.sif"
 export SLUMR_SERVER_DEFAULT_CONTAINER_LIB="merkel/jupyter:20240701"
 export SLUMR_SERVER_LOG_FOLDER="jupyter_notebook"
 
