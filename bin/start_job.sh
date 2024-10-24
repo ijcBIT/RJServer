4f6e67b060e8e0eac844ffc0f86fd53509637c3d
elario <elario@carrerasresearch.org>
2024-10-24 11:30:36 +0200

    Fix hang when the slurm job do not start

--- bin/start_job.sh
+++ bin/start_job.sh
@@ -22,12 +22,17 @@ while true; do
     JOB_STATE=$(squeue -j $JOB_ID -h -o '%T')
 
     if [[ "$JOB_STATE" == "RUNNING" ]]; then
+        # Print the slurm job id to cancel it when finished
+        echo "$JOB_ID"
         break  # Exit the loop
+    fi
+
+    # Check if the job has failed, completed, or been cancelled
+    if [[ "$JOB_STATE" == "FAILED" ]] || [[ "$JOB_STATE" == "CANCELLED" ]] || [[ "$JOB_STATE" == "COMPLETED" ]]  || [[ "$JOB_STATE" == "PREEMPTED" ]]; then
+        # Print the error message to stderr and exit with error
+        echo "Error: Job $JOB_ID has ended with state: $JOB_STATE" >&2
+        exit 1  # Exit with a non-zero error code
     fi
 
     sleep 1  # Wait before rechecking
 done
-
-# Print the slurm job id to cancel it when finished
-echo "$JOB_ID"
-
